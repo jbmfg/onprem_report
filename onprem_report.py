163a89e47d4e52965d00c85283fd10ec2110e938
Ben Goff <bengoff@gmail.com>
2022-08-09 16:16:40 -0400

    add subscriptions, work on opportunities

--- onprem_report.py
+++ onprem_report.py
@@ -49,7 +49,7 @@ class report_data(object):
         where i.installation_18_digit_id__c in ({self.inst_ids})
         """
         data = self.sfdb.execute(query)
-        fields = ("inst_id", "licenses_purchased", "normalized_host_count", "last_contact", "account_id", "product")
+        fields = ("inst_id", "licenses_purchased", "normalized_host_count", "last_contact", "acct_id", "product")
         self.db.update("installations", fields, data)
 
     def get_account_translation(self):
@@ -91,7 +91,8 @@ class report_data(object):
     def get_opportunity_info(self):
         accts = "'" + "', '".join(self.act_dict.keys()) + "'"
         query = f"""
-        select o.accountid,
+        select o.id,
+        o.accountid,
         o.acv_amount__c,
         o.cb_forecast__c,
         o.closeDate,
@@ -102,8 +103,31 @@ class report_data(object):
         and o.type like '%Renewal%'
         """
         data = self.sfdb.execute(query)
-        fields = ("acct_id", "acv", "forecast", "close_date", "type")
+        fields = ("opp_id", "acct_id", "acv", "forecast", "close_date", "type")
         self.db.insert("opportunities", fields, data)
+
+    def get_subscription_info(self):
+        accts = "'" + "', '".join(self.act_dict.keys()) + "'"
+        query = f"""
+        select account__c,
+        arr__c,
+        end_date__c,
+        id,
+        product_description__c,
+        product__c,
+        product_group__c,
+        quantity__c,
+        subscription_term__c,
+        tcv__c
+        from edw_tesseract.sbu_ref_sbusfdc.bit9_subscriptions__c s
+        where active_subscription__c = true
+        and account__c in ({accts})
+        """
+        data = self.sfdb.execute(query)
+        fields = ["acct_id", "arr", "end_date", "sub_id"]
+        fields += ["description", "product_id", "product"]
+        fields += ["quantity", "sub_term", "tcv"]
+        self.db.insert("subscriptions", fields, data)
 
     def renewal_quarter(self):
         def lookup_q(opp_date):
@@ -158,10 +182,10 @@ class report_data(object):
                         finding = f"{year} {q}"
             return finding
 
-        query = "select acct_id, close_date from opportunities;"
+        query = "select opp_id, close_date from opportunities;"
         data = self.db.execute(query)
         data = [[i[0], lookup_q(i[1])] for i in data]
-        fields = ("acct_id", "renewal_qt")
+        fields = ("opp_id", "renewal_qt")
         self.db.update("opportunities", fields, data)
 
     def deployment_percentage(self):
@@ -177,16 +201,18 @@ class report_data(object):
                 continue
             deployments.append([i[0], deployment])
         for i in deployments: print(i)
-        #data = [[i[0], round(i[1]/i[2] * 100, 2)] for i in data if i[1] is not None and i[2]]
         fields = ("inst_id", "deployment")
         self.db.update("installations", fields, deployments)
 
-    def acct_rollup(self):
-        print(json.dumps(self.act_dict, indent=2))
+    def product_family(self):
+        query = "select distinct type from opportunities;"
+        data = [i[0] for i in self.db.execute(query)]
+        products = set([i for prods in data for i in prods.split(";")])
+        for i in products: print(i)
 
 def table_creations():
     db = sqlite_db("onprem_products.db")
-    for table in ("installations", "accounts", "opportunities"):
+    for table in ("installations", "accounts", "opportunities", "subscriptions"):
         db.execute(f"drop table if exists {table};")
 
     # Installations
@@ -197,7 +223,7 @@ def table_creations():
     normalized_host_count INTEGER DEFAULT Null CHECK (typeof(normalized_host_count) in ('integer', Null)),
     deployment TEXT DEFAULT Null,
     last_contact STRING,
-    account_id STRING,
+    acct_id STRING,
     product STRING
     );
     """
@@ -223,12 +249,30 @@ def table_creations():
     # Opportunities
     query = """
     CREATE table opportunities(
+    opp_id TEXT PRIMARY KEY,
     acct_id TEXT,
-    acv CHECK (typeof(acv) in ('integer', Null)),
+    acv INTEGER CHECK (typeof(acv) in ('integer', Null)),
     forecast TEXT,
     close_date TEXT,
     renewal_qt TEXT,
     type TEXT
+    );
+    """
+    db.execute(query)
+
+    # Subscriptions
+    query = """
+    CREATE table subscriptions(
+    acct_id TEXT,
+    arr REAL CHECK (typeof(arr) in ('real')),
+    end_date TEXT,
+    sub_id TEXT,
+    description TEXT,
+    product_id TEXT,
+    product TEXT,
+    quantity INTEGER CHECK (typeof(quantity) in ('integer')),
+    sub_term INTEGER CHECK (typeof(sub_term) in ('integer')),
+    tcv REAL CHECK (typeof(tcv) in ('real'))
     );
     """
     db.execute(query)
@@ -287,6 +331,61 @@ def writerows(wb, sheet, data, linkBool=False, setwid=True, col1url=False, bolde
                 sheet.write_url(0, 6, "internal:Master!A1", string="Mastersheet")
         return True
 
+def create_inst_master(db):
+    def add_metric(metric):
+        pass
+    inst_ids = [i[0] for i in db.execute("select inst_id from installations;")]
+    rows = {i:{} for i in inst_ids}
+
+    # All of installations
+    data = db.execute_dict("select * from installations;")
+    for row in data:
+        row_tup = list(zip(row.keys(), [i for i in row]))
+        inst_id = row_tup.pop(0)[1]
+        rows[inst_id].update(dict(row_tup))
+
+    # All of accounts
+    query = """
+    select i.inst_id, a.*
+    from installations i
+    left join accounts a on i.acct_id = a.acct_id;
+    """
+    data = db.execute_dict(query)
+    for row in data:
+        row_tup = list(zip(row.keys(), [i for i in row]))
+        inst_id = row_tup.pop(0)[1]
+        rows[inst_id].update(dict(row_tup))
+
+    # Those opportunities that apply *CBLO can be multiple so its omitted + wtf is other?
+    lookup = {
+        "Cb Cloud": ["CBWL", "CBVM", "CBWS", "CBD", "CBCO", "CBTS", "CBTH"],
+        "Cb Response Cloud": ["CBRC"],
+        "Cb Protection": ["CBP"],
+        "Cb Response": ["CBR"]
+    }
+    for prod in lookup:
+        query = f"""
+        select i.inst_id,
+        o.close_date,
+        o.renewal_qt,
+        o.forecast,
+        o.acv as opp_acv,
+        count(*) as opp_count
+        from installations i
+        left join opportunities o on i.acct_id = o.acct_id
+        where i.product = '{prod}'
+        and o.type like '%{", ".join(lookup[prod])}%'
+        group by i.inst_id
+        having o.ROWID = min(o.ROWID)
+        order by o.close_date desc;
+        """
+        print(query)
+        data = db.execute(query)
+        for i in data:print(i)
+
+    #print(json.dumps(rows, indent=2))
+    return
+
 def write_report():
     db = sqlite_db("onprem_products.db")
     wb = xlsxwriter.Workbook("On-Prem Products_Consumption Report.xlsx")
@@ -308,16 +407,17 @@ def write_report():
         i.normalized_host_count as "Normalized Host Count",
         i.deployment as "Deployment %",
         i.last_contact as "Last Connection",
+        case when i.last_contact
         o.forecast as "Forecast",
         o.close_date as "Renewal Date",
         o.renewal_qt as "Renewal Quarter",
         i.inst_id
+        from installations i
         left join accounts a on i.account_id = a.acct_id
         left join opportunities o on i.account_id = o.acct_id
         where i.product = '{product}'
         and o.type like '%{type_lookup[product]}%';
         """
-        print(query)
         data = db.execute_dict(query)
         data = sorted(data, key=lambda x: x[0])
         header = data[0].keys()
@@ -329,14 +429,17 @@ def write_report():
     wb.close()
 
 if __name__ == "__main__":
+    create_inst_master(sqlite_db("onprem_products.db"))
+    import sys
+    sys.exit(1)
+    rd = report_data()
     table_creations()
     rd = report_data()
     rd.get_installation_info()
     rd.get_account_info()
     rd.get_opportunity_info()
+    rd.get_subscription_info()
     rd.renewal_quarter()
     rd.deployment_percentage()
-    rd.acct_rollup()
-    write_report()
-
-
+    rd.product_family()
+    #write_report()
